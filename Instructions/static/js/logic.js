7f31751a1df5c6d936e4d5decc4962843ee819ba
AnneNiemiec <anne_niemiec@outlook.com>
2021-07-11 18:14:23 -0700

    Added map layer

--- Instructions/static/js/logic.js
+++ Instructions/static/js/logic.js
@@ -1,18 +1,18 @@
-// Creating map object
-var myMap = L.map("mapid", {
-    center: [40.7, -73.95],
-    zoom: 11
-  });
-  
-  // Adding tile layer to the map
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-  }).addTo(myMap);
+    // Creating map object
+    var myMap = L.map("mapid", {
+        center: [40.7, -73.95],
+        zoom: 11
+    });
+    
+    // Adding tile layer to the map
+    L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/streets-v11",
+        accessToken: API_KEY
+    }).addTo(myMap);
   
     // Store API query variables
     var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
@@ -21,7 +21,7 @@ var myMap = L.map("mapid", {
     d3.json(baseURL, function(earthquakes) {
     console.log(earthquakes)
 
-    // .features.geometry
+    // .features.geometry (style)
     function createFeatures(earthquakesData){
         return {fillColor: earthquakeColor(earthquakesData.geometry.coordinates[2]),
             radius: earthquakeRadius(earthquakesData.properties.mag)}
@@ -47,10 +47,23 @@ var myMap = L.map("mapid", {
             return "#FFE4C4"            
         }
     }
-        // earthquakeRadius
+    // earthquakeRadius
     function earthquakeRadius(earthquakesData){
         return earthquakesData *2        
     }
+    // geojson
+    geojsonLayer = L.geoJson(earthquakesData, {
+        style: createFeatures,
+        pointToLayer: function(feature, latlng) {
+            return new L.CircleMarker(latlng
+            );
+        },
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.mag);
+        }
+    });
+    
+    map.addLayer(geojsonLayer);
      // Create a new marker cluster group
 
     // var markers = L.markerClusterGroup();
